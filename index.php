63a6c484f12c41938a4f98f8c5c0221b1b779292
yossandiimran <yossandiimran1307@gmail.com>
2023-01-14 19:30:18 +0700

    route + index

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+    
+    $project_location = "/kelurahanMargasari/index.php";
+    
+    $request = $_SERVER['REQUEST_URI'];
+    $me = $project_location;
+
+    try{
+        switch ($request) {
+            case $me.'/beranda' : require "view/beranda.php"; break;
+            default: http_response_code(404); echo "404"; break;
+        }
+    }catch(\err){
+        http_response_code(419);
+    }
+   
