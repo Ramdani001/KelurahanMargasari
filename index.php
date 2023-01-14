2cc435c7c090673784b6c324681e2924520e0c49
unknown <rizkan0990@gmail.com>
2023-01-14 19:52:13 +0700

    Route

--- index.php
+++ index.php
@@ -8,6 +8,12 @@
     try{
         switch ($request) {
             case $me.'/beranda' : require "view/beranda.php"; break;
+            case $me.'/ktp' : require "view/ktp.php"; break;
+            case $me.'/kk' : require "view/kk.php"; break;
+            case $me.'/kk' : require "view/kk.php"; break;
+            case $me.'/kematian' : require "view/kematian.php"; break;
+            case $me.'/pindah' : require "view/pindah.php"; break;
+            case $me.'/datang' : require "view/datang.php"; break;
             default: http_response_code(404); echo "404"; break;
         }
     }catch(\err){
