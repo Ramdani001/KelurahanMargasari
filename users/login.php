c89f01ed33a1e7fad7cede4a5f0e4fc984577c02
unknown <rizkan0990@gmail.com>
2023-01-14 17:29:21 +0700

    Page login users

---
+++ users/login.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Users</title>
+    <link rel="stylesheet" href="../assets/style/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../assets/style/bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="../assets/style/style.css">
+</head>
+<body>
+
+    <div class="container mt-5">
+        <div class="box m-auto w-75">
+            <div class="card p-5 w-50 m-auto text-center shadow">
+                <div class="card-header" style="margin-top: -30px;">
+                    <img src="../assets/img/logo.gif" width="80" alt="">
+                    <h3>Kelurahan</h3>
+                </div>
+                <div class="card-body">
+                    <form action="" method="post">
+                        <div class="input-group flex-nowrap mb-3">
+                            <span class="input-group-text" id="addon-wrapping">
+                                @
+                            </span>
+                            <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="addon-wrapping">
+                        </div>
+                        <div class="input-group flex-nowrap mb-3">
+                            <span class="input-group-text" id="addon-wrapping">
+                                p
+                            </span>
+                            <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="addon-wrapping">
+                        </div>
+                        <div class="">
+                            <button type="submit" class="btn btn-primary mb-2" class="" style="padding-left: 30px; padding-right: 30px;">
+                                Login
+                            </button>
+                            <br>
+                            <span>Anda Tidak Memiliki Akun? 
+                                <a href="register.php">Register</a>
+                            </span>
+                        </div>
+                    </form>
+                </div>
+                </div>
+
+        </div>
+    </divc>    
+
+<script src="../assets/style/bootstrap/js/bootstrap.js"></script>
+<script src="../assets/style/bootstrap/js/bootstrap.min.js"></script>
+<script src="../assets/style/bootstrap/js/bootstrap.bundle.js"></script>
+</body>
+</html>
