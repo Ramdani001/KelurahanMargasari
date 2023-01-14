9c4313a6a67ae6dee3afd6d50aa3eb2421561b3a
unknown <rizkan0990@gmail.com>
2023-01-14 17:52:00 +0700

    register

--- users/login.php
+++ users/login.php
@@ -5,9 +5,18 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Users</title>
+    
+    <!-- Bootstrap -->
     <link rel="stylesheet" href="../assets/style/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="../assets/style/bootstrap/css/bootstrap.css">
+    
+    <!-- MyStyle -->
     <link rel="stylesheet" href="../assets/style/style.css">
+
+    <!-- Fontawesome -->
+    <link rel="stylesheet" href="../assets/style/fontawesome.css">
+    <link rel="stylesheet" href="../assets/style/fontawesome.min.css">
+
 </head>
 <body>
 
@@ -15,31 +24,32 @@
         <div class="box m-auto w-75">
             <div class="card p-5 w-50 m-auto text-center shadow">
                 <div class="card-header" style="margin-top: -30px;">
-                    <img src="../assets/img/logo.gif" width="80" alt="">
+                    <img src="../assets/img/logoKel.png" width="80" alt="">
                     <h3>Kelurahan</h3>
                 </div>
                 <div class="card-body">
                     <form action="" method="post">
                         <div class="input-group flex-nowrap mb-3">
                             <span class="input-group-text" id="addon-wrapping">
-                                @
+                                <i class="fa-solid fa-user"></i>
                             </span>
-                            <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="addon-wrapping">
+                            <input type="text" class="form-control" placeholder="NIK" aria-label="nik" aria-describedby="addon-wrapping">
                         </div>
                         <div class="input-group flex-nowrap mb-3">
                             <span class="input-group-text" id="addon-wrapping">
-                                p
+                                <i class="fa-solid fa-lock"></i>
                             </span>
-                            <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="addon-wrapping">
+                            <input type="text" class="form-control" placeholder="Password" aria-label="password" aria-describedby="addon-wrapping" name="password">
                         </div>
                         <div class="">
                             <button type="submit" class="btn btn-primary mb-2" class="" style="padding-left: 30px; padding-right: 30px;">
                                 Login
                             </button>
-                            <br>
-                            <span>Anda Tidak Memiliki Akun? 
-                                <a href="register.php">Register</a>
-                            </span>
+                            <button type="button" class="btn btn-secondary mb-2" class="" style="padding-left: 30px; padding-right: 30px;">
+                                <a href="../index.html" class="text-decoration-none text-white">
+                                    Batal
+                                </a>
+                            </button>
                         </div>
                     </form>
                 </div>
@@ -48,8 +58,14 @@
         </div>
     </divc>    
 
-<script src="../assets/style/bootstrap/js/bootstrap.js"></script>
-<script src="../assets/style/bootstrap/js/bootstrap.min.js"></script>
-<script src="../assets/style/bootstrap/js/bootstrap.bundle.js"></script>
+    <!-- Bootstrap -->
+    <script src="../assets/style/bootstrap/js/bootstrap.js"></script>
+    <script src="../assets/style/bootstrap/js/bootstrap.min.js"></script>
+    <script src="../assets/style/bootstrap/js/bootstrap.bundle.js"></script>
+
+    <!-- Fontawesome -->
+    <script src="../assets/style/js/fontawesome.all.min.js"></script>
+    <script src="../assets/style/js/jquery-3.3.1.min.js"></script>
+
 </body>
 </html>
