e8520e21b97f3531afaf044f6b7876bae1c5b042
yossandiimran <yossandiimran1307@gmail.com>
2023-01-15 18:31:04 +0700

    Templating Layout

---
+++ view/layout/head.php
@@ -0,0 +1,21 @@
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Beranda | Kelurahan</title>
+    
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="<?php echo $main_url?>assets/style/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?php echo $main_url?>assets/style/bootstrap/css/bootstrap.css">
+    
+    <!-- MyStyle -->
+    <link rel="stylesheet" href="<?php echo $main_url?>assets/style/style.css">
+    
+    <!-- DataTables -->
+    <link rel="stylesheet" type="text/css" href="<?= $main_url?>assets/style/dataTables/jquerydataTables.min.css">
+    
+    <!-- Fontawesome -->
+    <link rel="stylesheet" href="<?php echo $main_url?>assets/style/fontawesome.css">
+    <link rel="stylesheet" href="<?php echo $main_url?>assets/style/fontawesome.min.css">
+    <link rel="stylesheet" href="<?php echo $main_url?>assets/style/style.css">
+</head>
---
+++ view/layout/navbar.php
@@ -0,0 +1,113 @@
+<nav class="navbar navbar-expand-lg bg-secondary position-relative shadow">
+    <div class="container-fluid">
+    <a class="navbar-brand ps-3" href="#">
+        <img src="../assets/img/logoKel.png" width="40" alt="">
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon">
+        <i class="fa-solid fa-bars"></i>
+        </span>
+    </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav d-flex w-100 justify-content-center">
+                <li class="nav-item active">
+                    <a class="nav-link " aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="<?= $main_url ?>index.php/ktp">KTP</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="<?= $main_url ?>index.php/kk">KK</a>
+                </li>
+                <li class="nav-item">
+                    <a href="<?= $main_url ?>index.php/kelahiran" class="nav-link">KELAHIRAN</a>
+                </li>
+                <li class="nav-item">
+                    <a href="<?= $main_url ?>index.php/kematian" class="nav-link">KEMATIAN</a>
+                </li>
+                <li class="nav-item">
+                    <a href="<?= $main_url ?>index.php/pindah" class="nav-link">PINDAH</a>
+                </li>
+                <li class="nav-item">
+                    <a href="<?= $main_url ?>index.php/datang" class="nav-link">DATANG</a>
+                </li>
+            </ul>
+            <?php if($dataLogin == null){?>
+                <li class="nav-item position-relative ps-2 bg-primary rounded" style="list-style: none;">
+                    <a href="<?= $main_url ?>index.php/login_user" class="nav-link text-center" style="text-decoration: none;">Login</a>
+                </li>
+            <?php }else{?>
+                <li class="nav position-relative ps-2 bg-primary rounded" style="list-style: none;">
+                    <div class="btn-group dropstart">
+                        <button type="button" class="btn text-light dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                            Profile
+                        </button>
+                        <ul class="dropdown-menu">
+                            <div class="container">
+                                <div class="col-md-12">
+                                    <span class="fa fa-user"></span>
+                                    &nbsp;<?=$dataLogin["namaLengkap"]?>
+                                </div>
+                            </div>
+                            <hr>
+                            <div class="container">
+                                <div class="col-md-12">
+                                    <a href="<?= $main_url ?>index.php/logout" class="dropdown-item text-center text-light" style="text-decoration: none; color: black !important;">Logout</a>
+                                </div>
+                            </div>
+                            <a href="#" data-bs-toggle="modal" data-bs-target="#exampleModal" class="dropdown-item cursor-pointer text-center text-light" style="text-decoration: none; color: black !important;">Ganti Password</a>
+                        </ul>
+                    </div>
+    
+                </li>
+            <?php }?>
+        </div>
+    </div>
+</nav>
+
+<!-- Modal Ganti Password-->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="exampleModalLabel">Ganti Password</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <form action="" method="poast" >
+                    <div class="input-group mb-3">
+                        <input type="password" class="form-control" id="pass" placeholder="Password Lama" name="passwordLama" aria-describedby="basic-addon2">
+                        <label for="pass" class="input-group-text" id="basic-addon2">
+                            <span id="show1" class="">
+                                <i class="fa-solid fa-eye-slash"></i>
+                            </span>
+                            <span id="show4" class="d-none">
+                                <i class="fa-solid fa-eye"></i>
+                            </span>
+                        </label>
+                    </div>
+                    <div class="input-group mb-3">
+                        <input type="password" class="form-control" id="passBaru" placeholder="Password Baru" name="passwordBaru" aria-describedby="basic-addon2">
+                        <label for="passBaru" class="input-group-text">
+                            <span id="show2" name="" class="show">
+                                <i class="fa-solid fa-eye-slash"></i>
+                            </span>
+                            <span id="show3" name="" class="d-none hide">
+                                <i class="fa-solid fa-eye"></i>
+                            </span>
+                        </label>
+                    </div>
+                    <div class="mb-3">
+                        <label for="pass" class="form-label">Konfirmasi Password</label>
+                        <input type="password" class="form-control" id="passBaru1" placeholder="Konfirmasi Password" name="confPass">
+                    </div>
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="submit" class="btn btn-primary">Save changes</button>
+            </div>
+        </div>
+    </div>
+</div>
+                
