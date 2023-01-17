1b420bc7c85e1a2ac3c9b3b3774ad854e55c3588
unknown <rizkan0990@gmail.com>
2023-01-17 11:43:01 +0700

    Change Project Location

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     
     // BASE URL SESUAIKAN DENGAN LINK / HOST MASING - MASING, TERMASUK PROJECT LOCATION NYA JUGA
     $base_url = "http://localhost"; // Sesuaikan dengan Host Masing - Masing
-    $project_location = "/kelurahan/index.php"; // Sesuaikan dengan nama project yg digunakan
+    $project_location = "/kelurahan"; // Sesuaikan dengan nama project yg digunakan
 
     // Global Variable
     $vars = explode("/",$_SERVER['REQUEST_URI']);
